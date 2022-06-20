2be9466c9efb836bf3734ae6e69dd9e67b406f16
Memel Thami <memelthami@gmail.com>
2022-06-20 16:50:44 +0100

    add store model

---
+++ backend/models/store.go
@@ -0,0 +1,9 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Store struct {
+	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name  string             `json:"name,omitempty" bson:"name,omitempty" validate:"required"`
+	Owner primitive.ObjectID `json:"owner,omitempty" bson:"owner,omitempty" validate:"required"`
+}
