b9b68d29116ca1bf142b2bd29c7177efd0a24a26
Memel Thami <memelthami@gmail.com>
2022-06-20 16:55:13 +0100

    Stores not required on every request

--- backend/routes/users.go
+++ backend/routes/users.go
@@ -3,11 +3,18 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/yrkan/pfa_sass_ecommerce/backend/controllers"
+	middlewares "github.com/yrkan/pfa_sass_ecommerce/backend/middleware"
 )
 
 func UsersRoute(route fiber.Router) {
-	route.Get("/", controllers.GetAllUsers)
-	route.Get("/:userId", controllers.GetSingleUser)
-	route.Post("/", controllers.CreateUser)
-	route.Delete(":/userId", controllers.DeleteUser)
+	// Get all users
+	route.Get("/", middlewares.Protected(), controllers.GetAllUsers)
+	// Get a single user
+	route.Get("/:userId", middlewares.Protected(), controllers.GetSingleUser)
+	// Create a user as admin
+	route.Post("/", middlewares.Protected(), controllers.CreateUser)
+	// Update a user as admin
+	route.Patch("/:userId", middlewares.Protected(), controllers.UpdateUser)
+	// Delete user
+	route.Delete(":/userId", middlewares.Protected(), controllers.DeleteUser)
 }
