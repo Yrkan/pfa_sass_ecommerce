3c277c37678d7e74bc47a7f8fce1863b6173a476
Memel Thami <memelthami@gmail.com>
2022-06-27 02:15:15 +0100

    make owner not required

--- backend/models/store.go
+++ backend/models/store.go
@@ -5,5 +5,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Store struct {
 	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name  string             `json:"name,omitempty" bson:"name,omitempty" validate:"required"`
-	Owner primitive.ObjectID `json:"owner,omitempty" bson:"owner,omitempty" validate:"required"`
+	Owner primitive.ObjectID `json:"owner,omitempty" bson:"owner,omitempty"`
 }
